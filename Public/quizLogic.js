d183550f2a0c9124f0fad7e5eded810d7fb4578e
amzahran <amzahran@azhar.edu.eg>
2025-05-10 04:09:46 +0300

    Delete Public directory

--- Public/quizLogic.js
+++
@@ -1,7 +0,0 @@
-
-// quizLogic.js
-// This is a basic placeholder to prevent missing script errors
-
-console.log("quizLogic.js loaded successfully.");
-
-// You can implement your test interaction logic here if needed
